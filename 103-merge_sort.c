6d56e465f0bf6c9fd8a91b0a7f08d738d2f6e0a3
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-09-25 21:52:51 +0200

    merge sort initialized

---
+++ 103-merge_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * merge_sort - top-down implementation of merge sort.
+ * @array: the array to be sorted.
+ * @size: size of the array.
+ */
+void merge_sort(int *array, size_t size)
+{
+
+}
