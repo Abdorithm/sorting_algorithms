2afcf67cde419ecea0c14f9980a4e6e97b4e946b
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-09-26 01:22:17 +0200

    finalizing

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,19 +1,62 @@
 #include "sort.h"
 
 /**
+ * document - documenting progress.
+ * @array: the sorted array.
+ * @splits: before sorting.
+ * @length_1: first partition length.
+ * @length_2: second partition length.
+ * @l: beginning of the partitions.
+ */
+void document(int *array, int *splits, int length_1, int length_2, int l)
+{
+	int i;
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	for (i = 0; i < length_1; i++)
+	{
+		printf("%i", splits[i]);
+		if (i < length_1 - 1)
+			printf(", ");
+	}
+	printf("\n");
+	printf("[Right]: ");
+	for (i = length_1; i < length_1 + length_2; i++)
+	{
+		printf("%i", splits[i]);
+		if (i < length_1 + length_2 - 1)
+			printf(", ");
+	}
+	printf("\n");
+	printf("[Done]: ");
+	for (i = l; i < l + length_1 + length_2; i++)
+	{
+		printf("%i", array[i]);
+		if (i < l + length_1 + length_2 - 1)
+			printf(", ");
+	}
+	printf("\n");
+	free(splits);
+}
+
+/**
  * merge - sort and merge partitions.
  * @array: the array.
  * @l: left index.
  * @m: mid.
  * @r: right index.
+ *
+ * Return: void.
  */
 void merge(int *array, int l, int m, int r)
 {
 	int i, j, k;
-	int length_1 = m - l + 1;
-	int length_2 = r - m;
+	int length_1 = m - l + 1, length_2 = r - m;
 	int *splits = (int *)malloc(sizeof(int) * (length_1 + length_2));
 
+	if (splits == NULL)
+		exit(EXIT_FAILURE);
 	for (i = 0; i < length_1; i++)
 		splits[i] = array[l + i];
 	for (j = 0; j < length_2; j++)
@@ -47,32 +90,7 @@ void merge(int *array, int l, int m, int r)
 		j++, k++;
 	}
 
-	printf("Merging...\n");
-	printf("[left]: ");
-	for (i = 0; i < length_1; i++)
-	{
-		printf("%i", splits[i]);
-		if (i < length_1 - 1)
-			printf(", ");
-	}
-	printf("\n");
-	printf("[Right]: ");
-	for (i = length_1; i < length_1 + length_2; i++)
-	{
-		printf("%i", splits[i]);
-		if (i < length_1 + length_2 - 1)
-			printf(", ");
-	}
-	printf("\n");
-	printf("[Done]: ");
-	for (i = l; i < l + length_1 + length_2; i++)
-	{
-		printf("%i", array[i]);
-		if (i < l + length_1 + length_2 - 1)
-			printf(", ");
-	}
-	printf("\n");
-	free(splits);
+	document(array, splits, length_1, length_2, l);
 }
 
 /**
@@ -80,6 +98,8 @@ void merge(int *array, int l, int m, int r)
  * @array: the array.
  * @l: left index.
  * @r: right index.
+ *
+ * Return: void.
  */
 void partition(int *array, int l, int r)
 {
