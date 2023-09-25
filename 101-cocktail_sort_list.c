824d1957710f5dc7def7168e9fb106576b426dd3
KENC0DE <kenansameseret@gmail.com>
2023-09-25 11:21:01 +0530

    fix

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -27,7 +27,6 @@ void swapit(listint_t *node, listint_t **list)
 void cocktail_sort_list(listint_t **list)
 {
 	listint_t *seak = *list;
-	listint_t *right = NULL, *left = NULL;
 	int swapped = TRUE;
 
 	if (!list || !(*list))
@@ -36,7 +35,7 @@ void cocktail_sort_list(listint_t **list)
 	while (swapped)
 	{
 		swapped = FALSE;
-		while (seak->next != right)
+		while (seak->next != NULL)
 		{
 			if (!seak->next)
 				break;
@@ -51,8 +50,8 @@ void cocktail_sort_list(listint_t **list)
 		}
 		if (!swapped)
 			break;
-		right = seak;
-		while (seak->prev != left)
+
+		while (seak->prev != NULL)
 		{
 			if (seak->n < seak->prev->n)
 			{
@@ -63,7 +62,6 @@ void cocktail_sort_list(listint_t **list)
 			else
 				seak = seak->prev;
 		}
-		left = seak;
 		seak = seak->next;
 	}
 }
